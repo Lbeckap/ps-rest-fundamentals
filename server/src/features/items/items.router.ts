18c89ace47cf53164b0d505bb527dff4a7cddba0
Lori <148814155+Lbeckap@users.noreply.github.com>
2025-05-10 16:15:11 +0000

    GET requests and zod validation

--- server/src/features/items/items.router.ts
+++ server/src/features/items/items.router.ts
@@ -1,6 +1,33 @@
 import express from "express";
+import { resolve } from "path";
+import { getItemDetail, getItems } from "./items.service";
+import { validate } from "../../middleware/validation.middleware";
+import { idItemIdUUIDRequestSchema, idNumberRequestSchema } from "../types";
+
 
 export const itemsRouter = express.Router();
+
+itemsRouter.get("/", async (req, res) => {
+  const items = await getItems();
+  items.forEach((item) => {
+    item.imageUrl = buildImageUrl(req, item.id);
+  });
+  res.json(items);
+});
+
+itemsRouter.get("/:id", validate(idNumberRequestSchema), async (req, res) => {
+  const data = idNumberRequestSchema.parse(req)
+  const item = await getItemDetail(data.params.id);
+  if (item != null){
+    item.imageUrl = buildImageUrl(req, item.id);
+    res.json(item);
+  } else {
+    res.status(404).json({message: "Item Not Found"});
+  }
+});
+
+
+
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 function buildImageUrl(req: any, id: number): string {
--- server/src/features/orders/orders.router.ts
+++ server/src/features/orders/orders.router.ts
@@ -1,3 +1,22 @@
 import express from "express";
+import { getOrderDetail, getOrders } from "./orders.service";
+import { idItemIdUUIDRequestSchema, pagingRequestSchema } from "../types";
+import { validate } from "../../middleware/validation.middleware";
 
 export const ordersRouter = express.Router();
+
+ordersRouter.get("/", validate(pagingRequestSchema), async (req, res) => {
+  const data = pagingRequestSchema.parse(req);
+  const orders = await getOrders(data.query.skip, data.query.take);
+  res.json(orders);
+});
+
+ordersRouter.get("/:id", validate(idItemIdUUIDRequestSchema), async (req, res) => {
+    const data = idItemIdUUIDRequestSchema.parse(req);
+    const order = await getOrderDetail(data.params.id);
+    if (order != null){
+        res.json(order);
+    } else {
+        res.status(404).json({message: "Order Not Found"});
+    }
+});
